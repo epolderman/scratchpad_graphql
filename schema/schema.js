42c6f0eb09ed64b318ed4aeab6ef10b0caed5cba
Erik Polderman <epolderman@averoinc.com>
2019-06-05 18:32:53 -0600

    set up edit, patch request

--- schema/schema.js
+++ schema/schema.js
@@ -115,7 +115,22 @@ const Mutation = new GraphQLObjectType({
         return axios
           .delete(`http://localhost:3000/users/${args.id}`, { id: args.id })
           .then(res => {
-            console.log('successful delete!');
+            return res.data;
+          });
+      }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .patch(`http://localhost:3000/users/${args.id}`, args)
+          .then(res => {
             return res.data;
           });
       }
